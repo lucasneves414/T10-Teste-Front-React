50c643dc2f974fae9896b99733735900f455a2a8
LUCAS NEVES <42455120+lucasneves414@users.noreply.github.com>
2021-02-21 11:32:05 -0300

    Feita pagina de serviços

--- src/Component/Array.js
+++ src/Component/Array.js
@@ -1,4 +1,6 @@
 import ImgHome from '../assets/trabalho-home.jpg';
+import Sobre from '../assets/sobre.jpg';
+import Art from '../assets/servicos-arte.jpg';
 
 export default
     {
@@ -13,8 +15,9 @@ export default
         {
             "tittle": "SOBRE NÓS",
             "tittle2": "Qual o seu próximo projeto?",
-            "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
-            "botao": "Download CV"
+            "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure",
+            "botao": "Download CV",
+            "img": {Sobre}
         },
 
         "Servicos":
@@ -29,9 +32,9 @@ export default
             },
 
             "content": {
-                "img": "",
+                "img": {Art},
                 "tittle": "Design",
-                "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+                "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
                 "botao": "Saiba mais"
             },
         },
--- src/Component/Content.jsx
+++ src/Component/Content.jsx
@@ -4,20 +4,69 @@ import props from './Array.js'
 export default function () {
     return (
         <div>
+
+            {/* HOME */}
             <div className="content">
-                <div className="content-tittle-subtittle">
+                <div className="content-tittle-subtittle-home">
                     {<h1>{props.Home.tittle}</h1>}
                     {<h3>{props.Home.subtittle}</h3>}
                 </div>
-                <div className="content-image">
+                <div className="content-image-home">
                     {<img src={props.Home.img.ImgHome} alt="Trabalho"></img>}
                 </div>
-                <div className="content-buttom-content">
+                <div className="content-buttom-content-home">
                     {<button>{props.Home.botao}</button>}
                 </div>
             </div>
 
-           
+            {/* SOBRE */}
+            <div className="content">
+                <div className="content-tittle-subtittle">
+                    {<h3>{props.SobreNos.tittle}</h3>}
+                </div>
+                <div className="content-space">
+                    <div className="content-image">
+                        <div className="grid-sobre"></div>
+                        {<img src={props.SobreNos.img.Sobre} alt="Trabalho"></img>}
+                    </div>
+                    <div className="content-tittle-card">
+                        {<h3>{props.SobreNos.tittle2}</h3>}
+                        {<h5>{props.SobreNos.content}</h5>}
+                        {<button>{props.Home.botao}</button>}
+                    </div>
+                </div>
+            </div>
+
+            {/* SERVIÇOS */}
+            <div className="content">
+                <div className="content-tittle-subtittle">
+                    {<h3>{props.Servicos.tittle}</h3>}
+                    {<h5>{props.Servicos.subtittle}</h5>}
+                </div>
+                <div className="content-space">
+                    <div className="content-left">
+                        <div className="content-left-card">
+                            <h4>{props.Servicos.contentUI.porcentage}</h4>
+                            <div className="container-progress-bar">
+                                <div className="progress-bar"></div>
+                            </div>
+                            <h3>{props.Servicos.contentUI.tittle}</h3>
+                            <h6>{props.Servicos.contentUI.subtittle}</h6>
+                        </div>
+                        <div className="grid-servicos"></div>
+                    </div>
+                    <div className="content-right">
+                    <div className="content-right-card">
+                    {<img src={props.Servicos.content.img.Art} alt="Trabalho"></img>}
+                        {<h3>{props.Servicos.content.tittle}</h3>}
+                        <div className="grid-servicos-title">_ _ _</div>
+                        {<h5>{props.Servicos.content.text}</h5>}
+                        {<button>{props.Servicos.content.botao}<i className='fa fa-long-arrow-right'></i></button>}
+                    </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
     )
 }
